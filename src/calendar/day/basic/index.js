f7474e277b1d9b2797040869af42abec5aecdc03
Hung Le <iu.developer232@yahoo.com>
2023-06-05 23:31:15 +0900

    Add

--- src/calendar/day/basic/index.js
+++ src/calendar/day/basic/index.js
@@ -75,8 +75,6 @@ const BasicDay = props => {
       }
     } else if (isDisabled) {
       styles.push(style.current.disabledText);
-      // Custom day text HungLe
-      styles.push({color: '#E5E5E5'});
     } else if (isToday) {
       styles.push(style.current.todayText);
     } else if (isInactive) {
